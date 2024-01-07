9c99460550b3656fdfbdbac039ab8d67ff72af72
DanielMena4 <129901636+DanielMena4@users.noreply.github.com>
2024-01-06 23:54:31 -0500

    Update farmacia.c

--- farmacia.c
+++ farmacia.c
@@ -1,74 +1,32 @@
-#include <stdio.h>
-#include <string.h>
-FILE *inventario;
-FILE *receta;
-struct datos_medicina
-{
-    char nombre[20];
-    char gramaje[10];
-    char precio[10];
-}datos;
-
+#include "funciones.h"
 int main()
 {
-    char addressinventario[]= "D:\\Nueva carpeta\\inventario.txt";
-    char addressreceta[]= "D:\\Nueva carpeta\\receta.txt";
-
-    int menu = 0;
-
-    inventario = fopen(addressinventario,"r");
-    if (inventario == NULL)
+    int opc;
+    do
     {
-        inventario = fopen(addressinventario,"a+");
-    }
-
-    receta = fopen(addressreceta,"r");
-    if (receta == NULL)
-    {
-        receta = fopen(addressreceta,"a+");
-    }
-
-    while (menu != 4)
-    {
-        printf("\n1. Mostrar el inventario\n2. Modificar el inventario\n3. Receta medica\n4. Salir\n");
-        scanf("%d", &menu);
-        switch (menu)
+        fflush(stdin);
+        printf("\n\t :MENU: \n");
+        printf("1.Crear\n2.Agregar\n3.Visualizar\n4.Salir\n");
+        printf("\nOpcion : ");
+        scanf("%i",&opc);
+        
+        switch (opc)
         {
         case 1:
+            crear();
             break;
         case 2:
-            printf("\n1. Añadir medicamento\n2. Modificar medicamento\n3. Eliminar medicamento\n");
-            scanf("%d",&menu);
-            switch (menu)
-            {
-            case 1:
-                fflush(stdin);
-                printf("\nNombre del medicamento : "); gets(datos.nombre);
-                printf("\nGramaje del medicamento : "); gets(datos.gramaje);
-                printf("\nPrecio del medicamento : "); gets(datos.precio);
-                fwrite(datos.nombre,1,strlen(datos.nombre),inventario);
-                fwrite(datos.gramaje,1,strlen(datos.gramaje),inventario);
-                fwrite(datos.precio,1,strlen(datos.precio),inventario);
-                break;
-            case 2:
-            break;
-            case 3:
-            break;
-            default:
-            printf("Ingrese una opcion correcta");
-                break;
-            }
+            agregar();
             break;
         case 3:
-            
-            break;        
+            visualizar();
+            break;
         case 4:
             break;
         default:
             printf("Ingrese una opcion valida");
             break;
         }
-    }
-    fclose(inventario);
+    } while (opc != 4);
     return 0;
 }
